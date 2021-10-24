5d79f990601b9fb77bf1966f781f11d89999889a
e.regas.duncan <e.regas.duncan@gmail.com>
2021-10-24 14:17:51 +0200

    clear redundant code

--- Main/Vector3.cpp
+++ Main/Vector3.cpp
@@ -1,10 +1,2 @@
 #include "Vector3.h"
 
-// Vector3::Vector3() : x(0), y(0), z(0){}
-
-/* Vector3::Vector3(const float& x_, const float& y_, const float& z_)
-{
-    x = x_;
-    y = y_;
-    z = z_;
-} */
